4a73d6013e40e6ca77d2362d7108c724e6d09e42
paperpatch <patchen21@gmail.com>
2022-02-05 22:26:59 -0500

    create thought list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
 
+import ThoughtList from '../components/ThoughtList';
+
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -12,8 +14,14 @@ const Home = () => {
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
       </div>
     </main>
   );
